2bf6d444612c440b92f95b7e80ea0bde0e38c5c9
Sam <sam.bogu@gmail.com>
2018-09-11 16:17:28 +0200

    add header componenent and basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header v-bind:mode="0" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Cabin:700|Nunito:300,700');
+
+// Reset
+a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
+
+// Constants
+:root {
+  --clr-text:         #231F20; // black
+  --clr-text-lt:      #EFF4F4; // ultra light blue
+
+  --clr-primary:      #6BBAEC; // cta blue
+  --clr-primary--lt:  #D4F5F5; // light blue
+  --clr-primary--dk:  #087E8B; // dark blue
+
+  --clr-secondary:    #37FF8B; // secondary cta green
+}
+
+// Global Styles
+* { box-sizing: border-box; }
+
+// Typography
+body {
+  color: var(--clr-text);
+  font-size: 16px;
+}
+h1, h2, h3, h4, h5, h6 { font-weight: bold }
+strong, b { font-weight: bold }
+a { color: var(--clr-primary--dk)}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Nunito", Helvetica, Arial, sans-serif;
+  // font-family: "Cabin", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  margin: auto;
+  max-width: 620px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <header v-bind:class="modes[mode].name">
+    {{modes[mode].title}}
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data: function(){
+    return {
+      modes: [
+        {
+          name: 'send',
+          title: 'Messenger'
+        },
+        {
+          name: 'respond',
+          title: 'Responding'
+        }
+      ]
+    }
+  },
+  props: {
+    mode: {
+      type: Number,
+      default: 0,
+      validator: function(value){
+        return value <= 1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  padding: 18px;
+  font-weight: bold;
+  font-size: 1.3em;
+  box-shadow: 0 1px 6px #ccc;
+
+  &.send {
+    background-color: white;
+  }
+  &.respond {
+    background-color: var(--clr-primary--dk);
+    color: var(--clr-text-lt);
+  }
+}
+</style>
